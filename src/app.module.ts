ed49340b3899551e8884f568634771e7baf68a7f
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-05 02:46:58 +0900

    mqtt서버의 기본 접속정보 등록 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import {Module} from '@nestjs/common';
 import {ClientsModule, Transport} from '@nestjs/microservices';
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
-
+//mqtt서버의 기본 접속 정보를 등록하는 곳
+//아이디와 비밀번호를 등록해줍니다.
 @Module({
   imports: [
       ClientsModule.register([
@@ -10,8 +11,10 @@ import {AppService} from "./app.service";
           name:'TEST_CLIENT',
           transport: Transport.MQTT,
           options: {
-            subscribeOptions:{ qos : 1 },
-            url: 'mqtt://broker.hivemq.com',
+            host: 'localhost',
+            port:1883,
+            clientId: 'id',
+            password:'password'
           }
         },
       ]),
@@ -19,7 +22,7 @@ import {AppService} from "./app.service";
   controllers: [AppController],
   providers:[AppService],
 })
-export class AppModule {}
-
-export class MqttModule {
+export class AppModule {
+  constructor() {
+  }
 }
