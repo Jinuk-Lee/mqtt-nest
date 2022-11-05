3f198e635ed59e12c1f80e3dbab0c82331e9d848
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-05 16:11:29 +0900

    mqtt 초기 구현

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,17 +4,17 @@ import {ClientProxy, MqttRecordBuilder} from "@nestjs/microservices";
 @Injectable()
 export class AppService {
     constructor(
-        @Inject('TEST_CLIENT') private client: ClientProxy,
+         @Inject('TEST_CLIENT') private client: ClientProxy,
     ) {
     }
 
-    sumDataService(payload: number[]) {
-        const response = payload.reduce((a, b) => a + b, 0);
-        const record = new MqttRecordBuilder(`${response}`)
-            .setQoS(1)
-            .build();
-        this.client.send('ftf-output', record).subscribe(res => {
-            console.log('response output: <', res, '>');
-        });
-    }
+    // sumDataService(payload: number[]) {
+    //     const response = payload.reduce((a, b) => a + b, 0);
+    //     const record = new MqttRecordBuilder(`${response}`)
+    //         .setQoS(1)
+    //         .build();
+    //     this.client.send('ftf-output', record).subscribe(res => {
+    //         console.log('response output: <', res, '>');
+    //     });
+    // }
 }
